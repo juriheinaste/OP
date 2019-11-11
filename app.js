6aac0e0fe5f38a4df0ad9b2a56b6a2a48b31f9f3
juriheinaste <juri.heinaste@khk.ee>
2019-11-11 11:23:35 +0200

    Window meetodid (funktsioonid)

---
+++ app.js
@@ -0,0 +1,16 @@
+// window objects / methods / properties
+
+// alert
+// window.alert('Tere hommikust!');
+// alert('Tere hommikust!');
+
+// prompt
+const sisend = prompt('Sisesta oma nimi');
+alert('Tere Hommikust, ' + sisend + '!');
+
+// confirm
+if(confirm('Kas oled nõus?')){
+    console.log('jah')
+} else {
+    console.log('ei');
+}
