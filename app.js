5e4e58e0349a539806e1f373738c182a0cd22403
juriheinaste <juri.heinaste@khk.ee>
2019-11-11 11:43:19 +0200

    Update

--- app.js
+++ app.js
@@ -5,12 +5,42 @@
 // alert('Tere hommikust!');
 
 // prompt
-const sisend = prompt('Sisesta oma nimi');
-alert('Tere Hommikust, ' + sisend + '!');
+// const sisend = prompt('Sisesta oma nimi');
+// alert('Tere Hommikust, ' + sisend + '!');
 
 // confirm
-if(confirm('Kas oled nõus?')){
-    console.log('jah')
-} else {
-    console.log('ei');
-}
+// if(confirm('Kas oled nõus?')){
+//    console.log('jah')
+// } else {
+//    console.log('ei');
+// }
+
+// välimõõdud
+// let valiKorgus = window.outerHeight;
+// let valiLaius = window.outerWidth;
+// console.log(valiKorgus, valiLaius);
+
+//sisemõõdud
+// let siseKorgus = window.innerHeight;
+// let siseLaius = window.innerWidth;
+// console.log(innerHeight, innerWidth)
+
+// scroll
+// scrollY = window.scrollY;
+// scrollX = window.scrollX;
+// console.log(scrollY, scrollX)
+
+// location object
+/*let objekt = window.location;
+let hostname = window.location.hostname;
+let port = window.location.port;
+let href = window.location.href;
+let search = window.location.search;
+console.log(hostname, port, href, search);
+*/
+
+// redirect
+// window.location.href = 'https://khk.ee'
+
+// reload
+// window.location.reload();
