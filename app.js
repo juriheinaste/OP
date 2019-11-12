556d942ed329444d5b547e142138390d7f9a2777
juriheinaste <juri.heinaste@khk.ee>
2019-11-12 15:37:34 +0200

    Sisu muutmine

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ klass = document.getElementById('add-task').className;
 
 sisu.style.background = '#333';
 sisu.style.padding = '5px';
+
+sisu.textContent = 'Minu ülesanded'
+sisu.innerText = 'Just minu ülesanded'
+sisu.innerHTML = '<span style="color:green">Vabad ülesanded</span>'
 console.log(sisu);
