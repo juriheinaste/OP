eaff8fa2d4a4e91f51a6abbbe9a07148240adf23
juriheinaste <juri.heinaste@khk.ee>
2019-11-18 10:52:06 +0200

    elementide loomine

--- app.js
+++ app.js
@@ -1,50 +1,25 @@
 // document methods
-// node omadused ja meetodid
-
-let nimekiri = document.querySelectorAll('ul.collection');
-let nimekirjaElement = document.querySelectorAll('li.collection-item');
-
-console.log(nimekiri);
-console.log(nimekirjaElement);
-
-nimekiri = document.querySelector('ul.collection');
-console.log(nimekiri);
-
-sisu = nimekiri.childNodes;
-sisu = nimekiri.childNodes[0];
-sisu = nimekiri.childNodes[0].nodeName;
-sisu = nimekiri.childNodes[3].nodeType;
-
-/*
-erinevad node tüübid
-1 - element
-2 - attribute
-3 - text
-8 - comment
-9 - document itself
-10 - doctype
-*/
-
-sisu = nimekiri.children;
-sisu = nimekiri.children[1];
-nimekiri.children[1].textContent = 'Õpi rohkem JS';
-sisu = nimekiri.children[2].children[0].id = 'katse';
-
-console.log(sisu);
-console.log(nimekiri.children[2].children[0]);
-
-sisu = nimekiri.firstChild; // node type
-sisu = nimekiri.firstElementChild; //value
-console.log(sisu);
-
-sisu = nimekiri.childElementCount;
-console.log(sisu);
-
-nimekirjaElement = document.querySelector('li.collection-item');
-sisu = nimekirjaElement.parentNode;
-sisu = nimekirjaElement.parentElement;
-sisu = nimekirjaElement.parentElement.parentElement;
-
-sisu = nimekirjaElement.nextSibling; // node type
-sisu = nimekirjaElement.nextElementSibling;
-console.log(sisu);
+// elementide loomine
+const liElement = document.createElement('li');
+// lisame klassi
+liElement.className = 'collection-item';
+// lisa id
+liElement.id = 'new-item';
+// lisa atribuut
+liElement.setAttribute('title', 'Uus element');
+// lisa teksti väljund
+// liElement.innerHTML = 'Uus element'
+liElement.textContent = 'Uus element';
+// loome lingi elemendi
+const link = document.createElement('a');
+// lisame vajalikud klassid
+link.className = 'delete-item secondary-content';
+// lisame kustutamisikooni
+link.innerHTML = '<i class="fa fa-remove"></i>';
+// lisame lingi listi elemendi sisse
+liElement.appendChild(link);
+// leiame ul elemendi DOM-ist
+ulElement = document.querySelector('ul.collection');
+// lisa liElemen ul elemendile
+ulElement.appendChild(liElement);
+console.log(liElement);
