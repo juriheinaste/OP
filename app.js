43feca403d132cfcb211c762ff39673c5940dd72
juriheinaste <juri.heinaste@khk.ee>
2019-11-20 09:53:12 +0200

    Sündmuste katsetamine

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 // sündmused
 
 const kustutaKoik = document.querySelector('.clear-tasks');
-kustutaKoik.addEventListener('click', vajutaNupp);
-
-function vajutaNupp(e){
-    let sisu = e;
-    // target
-    sisu = e.target;
-    sisu = e.target.id;
-    sisu = e.target.className;
-    sisu = e.target.classList;
-
-    // tüüp
-    sisu = e.type;
-
-    // sündmuse koordinaadid browseri akna suhtes
-    sisu = e.clientY;
-    sisu = e.clientX;
-
-    // sündmuse koordinaadid elemendi enda suhtes
-    sisu = e.offsetY;
-    sisu = e.offsetX;
-    console.log(sisu);
-
+const text = document.querySelector('h5')
+const kaart = document.querySelector('.card')
+// click
+// kustutaKoik.addEventListener('click', syndmus);
+// double click
+// kustutaKoik.addEventListener('dblclick', syndmus);
+// mous down
+// kustutaKoik.addEventListener('mousedown', syndmus);
+// mouse up
+// kustutaKoik.addEventListener('mouseup', syndmus);
+// mouse enter
+// kaart.addEventListener('mouseenter', syndmus)
+// mouse leave
+// kaart.addEventListener('mouseleave', syndmus)
+// mouse over
+// kaart.addEventListener('mouseover', syndmus)
+// mouse out
+// kaart.addEventListener('mouseout', syndmus)
+// mouse move
+// kaart.addEventListener('mousemove', syndmus)
+function syndmus(e){
+    // logi sünmuse tüüp
+    console.log(e.type);
+    // muuda teksti sisu: hiirX: x koord hiirY: y koord
+    // text.textContent = 'HiirX: ' + e.offsetX + 'HiirY ' + e.offsetY;
+    text.textContent = `HiirX:  ${e.offsetX} HiirY ${e.offsetY}`;
+    // koosta body värv rgb(Xkood, Ykood, 40)
+    document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 5)`
 }
