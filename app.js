68894fe28f5fcab84da0a5b1609e57d058930610
juriheinaste <juri.heinaste@khk.ee>
2019-11-20 10:58:44 +0200

    Sündmused - klaviatuuri sündmised

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ const text = document.querySelector('h5');
 // kustuta lisaUusYlesanne väärtus
 lisaUusYlesanne.value = '';
 
-// vormi saatimine
-form.addEventListener('submit', syndmus);
+// klaviatuur
+// key down
+// lisaUusYlesanne.addEventListener('keydown', syndmus);
+// key up
+// lisaUusYlesanne.addEventListener('keyup', syndmus);
+// key press
+// lisaUusYlesanne.addEventListener('keypress', syndmus);
+// focus
+// lisaUusYlesanne.addEventListener('focus', syndmus);
+// blur
+// lisaUusYlesanne.addEventListener('blur', syndmus);
+// paste
+// lisaUusYlesanne.addEventListener('paste', syndmus);
+// input
+lisaUusYlesanne.addEventListener('input', syndmus);
+
 
 function syndmus(e){
  
