089d81d5b9f89a1cdf392742cf7527d255026dba
juriheinaste <juri.heinaste@khk.ee>
2019-11-20 09:24:04 +0200

    Sündmus objekti omadused

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 // sündmused
 
 const kustutaKoik = document.querySelector('.clear-tasks');
-kustutaKoik.addEventListener('click', function(e){
-    console.log('Vajutatud');
-    // e.preventDefault();
-})
+kustutaKoik.addEventListener('click', vajutaNupp);
+
+function vajutaNupp(e){
+    let sisu = e;
+    // target
+    sisu = e.target;
+    sisu = e.target.id;
+    sisu = e.target.className;
+    sisu = e.target.classList;
+
+    // tüüp
+    sisu = e.type;
+
+    // sündmuse koordinaadid browseri akna suhtes
+    sisu = e.clientY;
+    sisu = e.clientX;
+
+    // sündmuse koordinaadid elemendi enda suhtes
+    sisu = e.offsetY;
+    sisu = e.offsetX;
+    console.log(sisu);
+
+}
