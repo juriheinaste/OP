04e7936df5e2a32ca8f56a4402df7d35e2f38a28
juriheinaste <juri.heinaste@khk.ee>
2019-12-11 15:09:41 +0200

    Ilma app Celsius

--- Weather.js
+++ Weather.js
@@ -1,17 +1,17 @@
-class Weather {
-    // konstruktor
-    constructor($c){
-        this.city = c;
-        this.key = 'b1744f623eae4972e39561fb4e30def4';
+class Weather{
+    // constructor
+    constructor(c){
+      this.city = c;
+      this.key = 'b1744f623eae4972e39561fb4e30def4';
     }
-
-    // data from API 588335
+  
+    // data from API
     async weatherData(){
-        const resp = fetch('https://api.openweathermap.org/data/2.5/weather?q=' + this.city + '&appid=' + this.key);  
-        const data = await resp.json();
-          console.log(data);
-          return data;
+      const resp = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+ this.city + '&appid=' + this.key);
+      const data = await resp.json();
+      console.log(data);
+      return data;
     }
+  
     // change city name
-
-}
+  }
