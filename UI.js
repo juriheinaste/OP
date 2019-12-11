5fdde7e32cb376f76473d0f8e3c0a107ef570050
juriheinaste <juri.heinaste@khk.ee>
2019-12-11 15:37:37 +0200

    Linna ilma info vahetamine on valmis

--- UI.js
+++ UI.js
@@ -1,15 +1,17 @@
 class UI {
     // constructor
     constructor(){
-        this.cityName = document.getElementById('w-city');
-        this.temp = document.getElementById('w-temp');
-        this.desc = document.getElementById('w-desc');
+      this.cityName = document.getElementById('w-city');
+      this.temp = document.getElementById('w-temp');
+      this.humidity = document.getElementById('w-humidity');
+      this.desc = document.getElementById('w-desc');
     }
-
+  
     print(cityWeather){
-        this.cityName.textContent = cityWeather.name;
-        const celsius = Math.round(parseFloat( cityWeather.main.temp)-273.15);
-        this.temp.innerHTML = `${celsius} &deg;C`;
-        this.desc.textContent = cityWeather.weather[0].description;
+      this.cityName.textContent = cityWeather.name;
+      const celcius = Math.round(parseFloat(cityWeather.main.temp)-273.15);
+      this.temp.innerHTML = `${celcius} &deg;C`;
+      this.humidity.innerHTML = `${cityWeather.main.humidity} %`;
+      this.desc.textContent = cityWeather.weather[0].description;
     }
-}
+  }
--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+  
 // weather for given location
 const weather = new Weather('Tartu linn');
 // app UI
 const ui = new UI();
 
-cityWeather = weather.weatherData().then(cityWeather => {
-  ui.print(cityWeather);
-}).catch(error => console.log(error));
+// change city - weather
+const changeBtn = document.getElementById('w-change');
+changeBtn.addEventListener('click', changeWeather);
+
+function changeWeather() {
+  const city = document.getElementById('city').value;
+  weather.changeCityName(city);
+  drawWeather();
+  $('#changeCity').modal('hide');
+  console.log(weather);
+}
+
+function drawWeather() {
+  weather.weatherData().then(cityWeather => {
+    ui.print(cityWeather);
+  }).catch(error => console.log(error));
+}
 
 console.log(ui);
