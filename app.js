405e5fa4dd98587383032ba9abb28eddb6a46943
juriheinaste <juri.heinaste@khk.ee>
2019-12-18 11:37:31 +0200

    Data Fetch  - github api lisatud

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 document.getElementById('btn1').addEventListener('click', getTextData);
 document.getElementById('btn2').addEventListener('click', getJsonData);
+document.getElementById('btn3').addEventListener('click', getJsonAPI);
 
+// remote API - remote JSON
+function getJsonAPI(){
+  fetch('https://api.github.com/users')
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(data){
+    let out = '';
+    data.forEach(function(users){
+      out = out + `<li>${users.login}</li>`;
+      out = out + `<img src="${users.avatar_url}" width="100">`;
+      console.log(out);
+    });
+    document.getElementById('out').innerHTML = out;
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+}
 // local Json file
 function getJsonData(){
   fetch('data.json')
