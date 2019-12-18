da5e4184f36e129b0cd61b2fb089b7233fefca34
juriheinaste <juri.heinaste@khk.ee>
2019-12-18 11:22:30 +0200

    Data Fetch - JSON nupp lisatud

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 document.getElementById('btn1').addEventListener('click', getTextData);
+document.getElementById('btn2').addEventListener('click', getJsonData);
 
+// local Json file
+function getJsonData(){
+  fetch('data.json')
+  .then(function(res){
+    console.log(res);
+    return res.json();
+  })
+  .then(function(data){
+    console.log(data);
+    let out = '';
+    data.forEach(function(timetable){
+      out = out + `<li>${timetable.opetaja} - ${timetable.aine}</li>`;
+      console.log(out);
+    });
+    document.getElementById('out').innerHTML = out;
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+}
+// local text file
 function getTextData() {
   fetch('text.txt')
   .then(function(res) {
